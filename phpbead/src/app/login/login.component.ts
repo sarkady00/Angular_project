42ca8be6f56edbb9f215ed624f3d653e736af233
sarkady00 <sarkadi.blazs@gmail.com>
2021-04-05 22:46:18 +0200

    login funkció(html, css, ts)
    Egyetlen felhasználó név és egy hozzá tartozó jelszóval lehet csak belépni, amit a projekt readme-jében talál

--- phpbead/src/app/login/login.component.ts
+++ phpbead/src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Logindata} from '../adat.model';
+import {FormGroup, FormBuilder} from '@angular/forms';
+import {Validators} from '@angular/forms';
+import { BeadservService} from '../beadserv.service';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  data = new Logindata();
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private readonly  fb: FormBuilder,
+    public service: BeadservService,
+    private route: Router
+  ) {
+    this.form = this.fb.group({
+      username: ['', [Validators.required, Validators.pattern('asdasd')]],
+      password: ['', [Validators.required, Validators.pattern('1234')]]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  submitForm(): void {
+    if (this.form.valid)
+    {
+      this.service.isLogged = true;
+      alert('Belépés sikeres');
+      this.route.navigate(['list']);
+    }
+    else{
+      alert('Hibás felhasználó név vagy jelszó');
+    }
+
   }
 
 }
