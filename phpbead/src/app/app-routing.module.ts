d4033584f0e7510ec475237b38a2befebc69ab80
sarkady00 <sarkadi.blazs@gmail.com>
2021-04-05 23:18:45 +0200

    Guard hozzáadása

--- phpbead/src/app/app-routing.module.ts
+++ phpbead/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {ListComponent} from './list/list.component';
 import {LoginComponent} from './login/login.component';
 import {FormComponent} from './form/form.component';
+import {RouteguardGuard} from './routeguard.guard';
 
 const routes: Routes = [
   {
@@ -17,11 +18,12 @@ const routes: Routes = [
   {
     path: 'list',
     component: ListComponent,
+    canActivate: [RouteguardGuard]
   },
   {
     path: 'form',
     component: FormComponent,
-    canActivate:
+    canActivate: [RouteguardGuard]
   }
 ];
 
--- phpbead/src/app/routeguard.guard.ts
+++ phpbead/src/app/routeguard.guard.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import {BeadservService} from './beadserv.service';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouteguardGuard implements CanActivate {
+
+  constructor(public service: BeadservService,
+              public route: Router) {
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    if (this.service.isLogged){
+      return true;
+    }
+    else{
+      this.route.navigate(['/login']);
+      return false;
+    }
   }
-  
 }
